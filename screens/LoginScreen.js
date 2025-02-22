78d65b80afda7c254c793b14e88edce2a195a1a9
Lee Sing Yuan <slee0163@student.monash.edu>
2025-02-22 22:42:59 +0800

    modified login screeen pending authentication

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,69 @@
 import React, { Component } from 'react'
-import { Text, View , SafeAreaView} from 'react-native'
+import { Text, View , SafeAreaView , Pressable } from 'react-native'
+import { LinearGradient } from "expo-linear-gradient";
+import Entypo from '@expo/vector-icons/Entypo';
 
-export default class LoginScreen extends Component {
-  render() {
-    return (
-        <linearGradient>
 
-        </linearGradient>
 
+const LoginScreen = () =>{
+    const authenticate = () => {
         
-        <SafeAreaView>
-        <Text> Login Screen </Text>
-        </SafeAreaView>
+    }
+
+
+    return (
+        <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }} >
+
+            <SafeAreaView>
+
+                <View style={{ height:80 }}>
+                    <Text> Login Screen </Text>
+                    <Entypo 
+                        style={{ textAlign: "center" }}
+                        name="spotify" 
+                        size={80} 
+                        color="white" />
+                </View>
+
+                <Text
+                    style={{
+                    color: "white",
+                    fontSize: 40,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    marginTop: 40,
+                    }}
+                    >
+                    Millions of Songs Free on spotify!
+                </Text>
+
+                
+                <View style={{ height: 80 }} />
+
+                <Pressable
+                onPress={authenticate}
+                style={{
+                backgroundColor: "#1DB954",
+                padding: 10,
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: 300,
+                borderRadius: 25,
+                alignItems: "center",
+                justifyContent: "center",
+                marginVertical:10
+                }}
+                >
+                    <Text>Sign In with spotify</Text>
+                </Pressable>
+
+            </SafeAreaView>
+
+        </LinearGradient>
+
+
+
     )
   }
-}
+
+export default LoginScreen
